2777e8ba98125ddab70a0c943e2c4e11ee25d364
Arthur Dorchies <166439257+ArtDorc@users.noreply.github.com>
2024-06-20 17:21:16 +0200

    Genre and Lastest connected to API

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const openModalBtn = document.querySelectorAll("#testreg");
 const closeModalBtn = document.querySelector(".btn-close");
-console.log(openModalBtn);
+/* console.log(openModalBtn); */
 // close modal function
 const closeModal = function () {
   modal.classList.add("hidden");
@@ -57,10 +57,7 @@ const openModal = function () {
   overlay.classList.remove("hidden");
 };
 // open modal event
-openModalBtn[0].addEventListener("click", openModal);
-openModalBtn[1].addEventListener("click", openModal);
-openModalBtn[2].addEventListener("click", openModal);
-openModalBtn[3].addEventListener("click", openModal);
+openModalBtn.forEach(item => item.addEventListener("click", openModal));
 
 // Select the element with the class "Signup" and store it in the variable Signup
 const Signup = document.querySelector(".Signup");
@@ -87,12 +84,12 @@ const Log = function () {
 // Add a click event listener to the Login element that calls the Log function when clicked
 Login.addEventListener("click", Log);
 
-let navlinksgenres = document.getElementsByClassName("nav-links-genres")[0];
+/* let navlinksgenres = document.getElementsByClassName("nav-links-genres")[0];
 let Comedy = document.querySelector('a[href="#Comedy"]');
 let Drama = document.querySelector('a[href="#Drama"]');
 let Action = document.querySelector('a[href="#Action"]');
 let Fantasy = document.querySelector('a[href="#Fantasy"]');
-let Animation = document.querySelector('a[href="#Animation"]');
+let Animations = document.querySelector('a[href="#Animation"]');
 let swiperP = document.getElementsByClassName("swiperP")[0];
 
 const clickGenres = function () {
@@ -100,10 +97,10 @@ const clickGenres = function () {
 };
 
 Comedy.addEventListener("click", clickGenres);
-Animation.addEventListener("click", clickGenres);
+Animations.addEventListener("click", clickGenres);
 Drama.addEventListener("click", clickGenres);
 Action.addEventListener("click", clickGenres);
-Fantasy.addEventListener("click", clickGenres);
+Fantasy.addEventListener("click", clickGenres); */
 
 let Searchinput = document.getElementById("searchinput");
 let buttonSearch = document.getElementById("buttonSearch");
@@ -111,7 +108,7 @@ let Searchinput3 = " ";
 
 const logInputValue = function () {
   Searchinput3 = Searchinput.value;
-  console.log(Searchinput3);
+/*   console.log(Searchinput3); */
   spanElement.textContent = Searchinput3;
 };
 
@@ -127,10 +124,10 @@ let spanElement = searchDiv.querySelector("p span");
 
 let imgContainer = document.getElementsByClassName("image-container");
 
-console.log(imgContainer);
+/* console.log(imgContainer); */
 
 const modalCardF = function (event) {
-  console.log(event.target);
+/*   console.log(event.target); */
   modalOpencard();
 };
 
@@ -142,17 +139,216 @@ let modalcard = document.querySelector(".modalcard");
 
 function modalOpencard() {
   modalcard.classList.remove("hiddencard");
-  console.log(modalcard);
+  /* console.log(modalcard); */
 }
 
 function modalClosecard() {
   modalcard.classList.add("hiddencard");
 }
-
-let LO = document.querySelectorAll(".image-container");
-Array.from(LO).forEach(function (e) {
-  e.addEventListener("click", modalCardF);
-});
-
+function addClickOnMovies () {
+  let LO = document.querySelectorAll(".image-container");
+  Array.from(LO).forEach(function (e) {
+    e.addEventListener("click", modalCardF);
+  });
+}
+addClickOnMovies();
 const closeBtn = document.querySelector(".btn-closecard");
 closeBtn.addEventListener("click", modalClosecard);
+
+      //////     START API PART /////////
+
+const options = {
+  method: 'GET',
+  headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OGQ0NjAxOGY4MjVjOTdkMTM2ZWQ3ZDEyMzZiZTZmYyIsInN1YiI6IjY2NmZlMjVhMTM1ZDM5MTdmMTU0NzA1MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yBakHCKugpBK7pQtkIV_rvYAqZpKB7c0zTs8qQ5bVGM'
+  }
+}; 
+
+const err = `An error has occured during your connection to API`;
+
+/* fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+*/
+const searchWrapper = document.querySelector("#searchSwiper").querySelector(".swiper-wrapper");
+const lastestWrapper = document.querySelector("#lastestSwiper").querySelector(".swiper-wrapper");
+const genreWrapper = document.querySelector("#genreSwiper").querySelector(".swiper-wrapper");
+const navLinks = document.querySelector(".nav-links-genres");
+navLinks.querySelectorAll("a").forEach(item => item.addEventListener("click",onClickGenre));
+
+
+let genreIdList;
+const baseUrl="https://image.tmdb.org/t/p/w300";
+
+//For any name genre return the id
+function getIdforGenre (genre) {
+  let idGenre;
+  genreIdList.forEach(item => {
+      if (item.name == genre) {
+          idGenre = item.id;
+      }
+      })
+      if (idGenre) { 
+          return idGenre;
+      } else {
+          console.log(`The genre ${genre} has no id in the list`);
+      }
+}
+//For any idgenre return the genre
+function getGenreforId (idgenre) {
+  let genre;
+  genreIdList.forEach(item => {
+      if (item.id == idgenre) {
+          genre = item.name;
+      }
+      })
+      if (genre) { 
+          return genre;
+      } else {
+          console.log(`The genre ${idgenre} has no name in the list`);
+      }
+}
+
+//fectch the ID of every genre and store it in GenreIdList
+function getGenreIdList() {
+  return fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options)
+      .then(data => data.json())
+      .then(data => {
+          return data.genres;
+      })
+      .catch(err => console.error("failed to fetch the GenreIdList"));
+  }
+
+//function that reset the swipers
+function resetSwiper() {
+  const swiper = new Swiper(".swiper", {
+      // Optional parameters
+      direction: "horizontal",
+      loop: true,
+      spaceBetween: 40,
+      centeredSlides: false,
+      slidesPerGroup: 1,
+      slidesPerView: 1,
+    //   slidesOffsetAfter: 100,
+    //   slidesOffsetbefore: 100,
+      breakpoints: {
+        500: {
+          slidesPerView:2,
+        },
+        768: {
+          slidesPerView:3,
+        },
+        1250: {
+          slidesPerView:4,
+        },
+      },
+    
+      // Navigation arrows
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+}
+//function that activate on the click on a genre
+
+function onClickGenre(e) {
+  if (e) {
+      e.preventDefault();
+  }
+  navLinks.querySelectorAll("a").forEach(item =>item.classList.remove("activeLink"));
+  e.target.classList.add("activeLink");
+  let genre = e.target.innerText;
+  displayGenre(genre);
+}
+//function that create an array of movie's formatted HTML based on the results;
+function createMovieHTMLArray (results) {
+  let htmlArray =[];
+  results.forEach(movie =>{
+    let title = movie.title;
+    let image = baseUrl+movie.poster_path;
+    let date = new Date(movie.release_date);
+    let year = date.getFullYear();
+    let categories ="";
+        let indexId=0;
+        movie.genre_ids.forEach(item => {
+            indexId++;
+            if (indexId==1) {
+                categories += getGenreforId(item)
+            } else {
+                categories += " / "+ getGenreforId(item)
+            }
+            });
+    let rating = Math.round(movie.vote_average*10)/10;
+    let id=movie.id;
+
+    htmlArray.push(
+      `
+<div class="swiper-slide" data-movieId="${id}">
+    <div class="image-container">
+        <img src="${image}" alt="">
+        <div class="overlayD">
+            <div class="text">
+                <h2 class="title">${title}</h2>
+                <p class="date">${year}</p>
+                <p class="categories">${categories}</p>
+                <div class="ratings">
+                    <img src="star.svg" alt="">
+                    <p class="rating">${rating}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+      `)
+  })
+  return htmlArray;
+}
+async function displayGenre(genre) {
+  let genreID = await getIdforGenre(genre);
+  let results = await fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${genreID}`, options)
+      .then(response => response.json())
+      .then(response => {
+          return response.results;
+      })
+      .catch(err => console.error(err));
+  document.querySelector(".swiperP").innerText=genre;
+  genreWrapper.innerHTML="";
+  createMovieHTMLArray(results).forEach(item => genreWrapper.innerHTML+=item);
+  resetSwiper();
+  addClickOnMovies();
+}
+
+async function displayLastest() {
+  const todayDate =new Date().toJSON().slice(0, 10);
+  let results = await fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&primary_release_date.lte=${todayDate}&include_video=false&language=en-US&page=1&sort_by=popularity.desc&vote_count.gte=20`, options)
+    .then(response => response.json())
+    .then(response => {
+      return response.results;
+    })
+    .catch(err => console.error("Error on connecting with the API for Lastest Movies."));
+    lastestWrapper.innerHTML="";
+    createMovieHTMLArray(results).forEach(item => lastestWrapper.innerHTML+=item);
+    resetSwiper();
+    addClickOnMovies();
+}
+async function displayResult (research) {} {
+  fetch('https://api.themoviedb.org/3/search/movie?query=godfather&include_adult=false&language=en-US&page=1', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+}
+
+//function that will run at the start
+async function initalisation () {
+  genreIdList = await getGenreIdList();
+  displayLastest();
+  let firstGenre = navLinks.querySelector("a");
+  firstGenre.classList.add("activeLink");
+  displayGenre(firstGenre.innerText);
+}
+
+initalisation();
+
